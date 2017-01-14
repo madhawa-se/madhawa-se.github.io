70467a951379c21f74d4635205f09189ac76253c
madhawa-se <madhawapriyashantha@gmail.com>
2017-01-15 02:43:25 +0530

    adding 2nd sample

--- js/app.js
+++ js/app.js
@@ -1,25 +1,23 @@
 function animatePro() {
     var elem = $(".skill-graph");
-    var pskill = [50, 60, 20, 40, 60];
+    var pskill = [50, 40, 45, 40, 50, 20, 30];
     for (var i = 0; i < elem.length; i++) {
         //e.eq(i).css("background-color",col[i]);
-        elem.eq(i).animate({width: pskill[i] + "%"}, 3000, 'swing');
+        elem.eq(i).animate({width: pskill[i] + "%"}, 2000, 'swing');
     }
 }
 $(document).ready(function () {
 
     var waypoint = new Waypoint({
-        element: document.getElementsByClassName('skills')[0],
+        element: document.getElementsByClassName('section4')[0],
         handler: function () {
             animatePro();
         },
         offset: 80
     });
-    var waypoint1 = new Waypoint({
-        element: document.getElementsByClassName('section6')[0],
-        handler: function () {
-            $('.send').addClass('animated zoomIn');
-        }
-    });
+
+    $('body').animate({
+       // scrollTop: $(".middle").offset().top
+    }, 2000);
 
 });
